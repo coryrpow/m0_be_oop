e3b0802fb0c94951e57e67961b9003f6e37f4150
Cory Powell <coryrpowell@gmail.com>
2023-05-21 15:57:40 -0400

    Add up to dragon

--- oop.rb
+++ oop.rb
@@ -2,15 +2,43 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
-
-
-
+class Unicorn
+    attr_reader :name, :color
+    def initialize(name, color = "silver")
+        @name = name
+        @color = color
+    end
+    def say
+        "*~*#{name}*~*"
+    end
+end
+Sufjan = Unicorn.new("Sufjan")
+puts Sufjan.color
+puts Sufjan.say
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+    attr_reader :name, :pet, :thirsty, :drink
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+        @drink = true
+    end
+    def drink
+        @drink = false
+    end
+end
 
+Edward = Vampire.new("Edward", "cat")
+p Edward
+Edward.drink
+p Edward
+Dracula = Vampire.new("Dracula")
+p Dracula
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,7 +46,18 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+   attr_reader :name, :rider, :color, :is_hungry, :eat
+   def initialize(name, rider, color, is_hungry)
+       @name = name
+       @rider = rider
+       @color = color
+       @is_hungry = true
+   end
+end
 
+Puff = Dragon.new("Puff", "Lenny", "green", 3)
+p Puff
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -27,4 +66,4 @@
 #  it should have a celebrate_birthday method. When called, the age increases by 1
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
-#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+#  it should have a has_ring attribute. If the Hobbit's name is "Frodo, true, if not, false.
